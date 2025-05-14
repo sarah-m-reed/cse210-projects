d79b2a510a2d6523a171121446c80bffa1740d11
sarah-m-reed <sarah@duckworthfamily.org>
2025-05-14 13:55:17 -0600

    Completed Abstract Learning Activity

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,14 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear = 0;
+    public int _endYear = 0;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
