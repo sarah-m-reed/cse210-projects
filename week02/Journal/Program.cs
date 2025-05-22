1f50f9266e783795f8b65997846addef204b005b
sarah-m-reed <sarah@duckworthfamily.org>
2025-05-22 11:33:18 -0600

    Completed Encapsulation Learning Activity

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
         Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
