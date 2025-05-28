62f06457b1f15301d4963b3ad24bc55f0a4842e9
sarah-m-reed <sarah@duckworthfamily.org>
2025-05-27 20:35:03 -0600

    Completed Scripture Memorizer Project

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,2 +1,33 @@
 public class Reference
-{}
+{
+    private string _book = "";
+    private int _chapter = 0;
+    private int _verse = 0;
+    private int _endVerse = 0;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = 0;
+    }
+    public Reference(string book, int chapter, int verse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+    public string GetDisplayText()
+    {
+        if (_endVerse != 0 && _endVerse != _verse)
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+    }
+}
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,2 +1,63 @@
 public class Scripture
-{}
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitWords = text.Split(' ');
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+
+        List<int> visibleIndexes = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            if (!_words[i].IsHidden())
+            {
+                visibleIndexes.Add(i);
+            }
+        }
+
+        while (hiddenCount < numberToHide && visibleIndexes.Count > 0)
+        {
+            int randomIndex = random.Next(visibleIndexes.Count);
+            int wordIndex = visibleIndexes[randomIndex];
+            _words[wordIndex].Hide();
+            visibleIndexes.RemoveAt(randomIndex);
+            hiddenCount++;
+        }
+    }
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+
+        foreach (Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+
+        return $"{_reference.GetDisplayText()} {string.Join(" ", displayWords)}";
+    }
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
